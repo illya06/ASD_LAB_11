e93c83e30b053825b5c837dd3b2d153262e4c077
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-19 13:14:09 +0200

    test-edited

--- ASD_LAB_11/KMP_L.cs
+++ ASD_LAB_11/KMP_L.cs
@@ -8,9 +8,8 @@ namespace ASD_LAB_11
 {
     class KMP_L
     {
-        public static string find(string text, string pat)
+        public static int find(string text, string pat)
         {
-            string log = "";
             int i, j, N, M;
 
             N = text.Length;
@@ -19,19 +18,20 @@ namespace ASD_LAB_11
 
             d[0] = 0;
 
-            for(i = 0, j = 0; i < M; i++)
+            for (i = 0, j = 0; i < M; i++)
             {
-                while(j>0 && pat[j] != pat[i])
+                while (j > 0 && pat[j] != pat[i])
                 {
                     j = d[j - 1];
                 }
-                if(pat[j] == pat[i])
+                if (pat[j] == pat[i])
                 {
                     j++;
                 }
                 d[i] = j;
             }
 
+            //search
             i = 0; j = 0;
             while (i < N - j)
             {
@@ -45,10 +45,10 @@ namespace ASD_LAB_11
                 }
                 if (j == M)
                 {
-                    log += $"\n At ({i - j + 1}) : Entry of {pat}";
+                    return i - j + 1;
                 }
             }
-            return log;
+            return -1;
         }
     }
 }
