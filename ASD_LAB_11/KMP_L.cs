cd022664b39ba005e6f76910e0a739a0ef9e0f51
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-19 12:49:19 +0200

    Added_correct_KMP_class

---
+++ ASD_LAB_11/KMP_L.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD_LAB_11
+{
+    class KMP_L
+    {
+        public static int find(string text, string pat)
+        {
+            int i, j, N, M;
+
+            N = text.Length;
+            M = pat.Length;
+            int[] d = new int[M];
+
+            d[0] = 0;
+
+            for(i = 0, j = 0; i < M; i++)
+            {
+                while(j>0 && pat[j] != pat[i])
+                {
+                    j = d[j - 1];
+                }
+                if(pat[j] == pat[i])
+                {
+                    j++;
+                }
+                d[i] = j;
+            }
+
+            i = 0; j = 0;
+            while (i < N - j)
+            {
+                while (j > 0 && pat[j] != text[i])
+                {
+                    i = i + j - d[j - 1] - 1;
+                }
+                if (pat[j] == text[i])
+                {
+                    j++;
+                }
+                if (j == M)
+                {
+                    return i - j + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
